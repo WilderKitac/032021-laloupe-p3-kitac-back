71997aa3b78c2d0ba0ec85f45b9d4aa6f03faf95
Damien Landouard <dlandouard@gmail.com>
2021-06-16 12:27:09 +0200

    résolution conflits

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,11 @@
 const mainRouter = require('express').Router();
 const usersRoutes = require('./users.routes');
-<<<<<<< HEAD
 const userTypesRoutes = require('./userTypes.routes');
 const productsRoutes = require('./products.routes');
 
 mainRouter.use('/users', usersRoutes);
 mainRouter.use('/userTypes', userTypesRoutes);
 mainRouter.use('/products', productsRoutes);
-=======
 const categoryRoutes = require('./category.routes');
 const suppliesRoutes = require('./supplies.routes');
 const sizeRoutes = require('./size.routes');
@@ -16,6 +14,5 @@ mainRouter.use('/users', usersRoutes);
 mainRouter.use('/categories', categoryRoutes);
 mainRouter.use('/supplies', suppliesRoutes);
 mainRouter.use('/size', sizeRoutes)
->>>>>>> dev
 
 module.exports = mainRouter;
