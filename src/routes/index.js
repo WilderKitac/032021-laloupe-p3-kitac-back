0e465255ea34359cbb2c0156aec0c9dfc2d12217
abbas <abbas.alex1147@gmail.com>
2021-06-16 13:50:28 +0200

    creation routes productImages

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const mainRouter = require('express').Router();
 const usersRoutes = require('./users.routes');
+const imagesRoutes = require('./productsImages.routes');
 
 mainRouter.use('/users', usersRoutes);
+mainRouter.use('/productsimages', imagesRoutes);
 
 module.exports = mainRouter;
