016b4b08771c27aec197fe6ac6c4190e13dae892
Damien Landouard <dlandouard@gmail.com>
2021-07-09 10:29:39 +0200

    modification expiration

--- src/services/jwt.js
+++ src/services/jwt.js
@@ -10,7 +10,7 @@ const createToken = (req, res) => {
     expiresIn: JWT_LIFETIME,
   });
   const refreshToken = jwt.sign({ id: req.user ? req.user.id : 0, role: req.user.user_types_id }, REFRESH_JWT_SECRET, { expiresIn: 90 });
-  res.cookie('refresh_token', refreshToken, { maxAge: 60000 });
+  res.cookie('refresh_token', refreshToken, { maxAge: 3600000 });
   res.json({ id: req.user ? req.user.id : 0, role: req.user.user_types_id, token });
 };
 
