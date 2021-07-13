fd3f298d5260e6f0f3c36cacde0e1b3835d5d4bc
Damien Landouard <dlandouard@gmail.com>
2021-07-13 12:20:21 +0200

    ajout multer route matières

--- src/controllers/materials.controller.js
+++ src/controllers/materials.controller.js
@@ -1,4 +1,5 @@
 const Joi = require('joi');
+const multer = require('multer');
 const { findMany, findOneById, createOne, updateOne, deleteOne, findMaterialsPerProductId } = require('../models/materials.model');
 
 const getAllMaterials = (req, res) => {
@@ -34,17 +35,18 @@ const getOneMaterialsById = (req, res) => {
 };
 
 const createOneMaterials = (req, res, next) => {
-  const { material_type, material_price, quantity, API_Mat_id } = req.body;
+  const { material_type, material_price, quantity, API_Mat_id, image } = req.body;
   const { error } = Joi.object({
     material_type: Joi.string().max(100).required(),
     material_price: Joi.number().precision(2).required(),
     quantity: Joi.number().integer().required(),
     API_Mat_id: Joi.number().integer(),
-  }).validate({ material_type, material_price, quantity, API_Mat_id }, { abortEarly: false });
+    image: Joi.string().max(255),
+  }).validate({ material_type, material_price, quantity, API_Mat_id, image }, { abortEarly: false });
   if (error) {
     res.status(422).json({ validationErrors: error.details });
   } else {
-    createOne({ material_type, material_price, quantity, API_Mat_id })
+    createOne({ material_type, material_price, quantity, API_Mat_id, image })
       .then(([results]) => {
         res.status(201);
         req.materialsId = results.insertId;
@@ -54,6 +56,53 @@ const createOneMaterials = (req, res, next) => {
         res.status(500).send(err.message);
       });
   }
+};
+
+const createAssetMaterial = (req, res, next) => {
+  const { material_type, material_price, quantity, API_Mat_id, image } = req.body;
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'public/image');
+    },
+    filename: (_, file, cb) => {
+      cb(null, `${Date.now()}-${file.originalname}`);
+    },
+  });
+
+  const upload = multer({ storage: storage }).single('file');
+  upload(req, res, (err) => {
+    if (err) {
+      res.status(500).json(err);
+    } else {
+      let validationData = null;
+      validationData = Joi.object({
+        material_type: Joi.string().max(100).required(),
+        material_price: Joi.number().precision(2).required(),
+        quantity: Joi.number().integer().required(),
+        API_Mat_id: Joi.number().integer(),
+        image: Joi.string().max(255),
+      }).validate({ material_type, material_price, quantity, API_Mat_id, image }, { abortEarly: false }).error;
+
+      if (validationData) {
+        console.log(validationData);
+        res.status(500).send('Données invalides');
+      } else {
+        console.log(req.file.filename);
+        console.log(req.body.configuration);
+        const configuration = JSON.parse(req.body.configuration);
+        console.log(configuration);
+        req.project = {
+          image: req.file.filename,
+          ...configuration,
+        };
+        console.log(req.project);
+        createOne(req.project).then((result) => {
+          req.projectId = result[0].insertId;
+          next();
+        });
+      }
+    }
+  });
 };
 
 const updateOneMaterials = (req, res, next) => {
@@ -123,6 +172,7 @@ module.exports = {
   getAllMaterials,
   getOneMaterialsById,
   createOneMaterials,
+  createAssetMaterial,
   updateOneMaterials,
   deleteOneMaterials,
   getMaterialsByProductId,
--- src/routes/materials.routes.js
+++ src/routes/materials.routes.js
@@ -1,10 +1,19 @@
 const materialsRouter = require('express').Router();
-const { getAllMaterials, getOneMaterialsById, createOneMaterials, updateOneMaterials, deleteOneMaterials } = require('../controllers/materials.controller');
+const {
+  getAllMaterials,
+  getOneMaterialsById,
+  createOneMaterials,
+  createAssetMaterial,
+  updateOneMaterials,
+  deleteOneMaterials,
+} = require('../controllers/materials.controller');
 
 materialsRouter.get('/', getAllMaterials);
 materialsRouter.get('/:id', getOneMaterialsById);
 materialsRouter.post('/', createOneMaterials, getOneMaterialsById);
 materialsRouter.put('/:id', updateOneMaterials, getOneMaterialsById);
 materialsRouter.delete('/:id', deleteOneMaterials);
+materialsRouter.post('/', createOneMaterials, getOneMaterialsById);
+materialsRouter.post('/Create', createAssetMaterial, getOneMaterialsById);
 
 module.exports = materialsRouter;
