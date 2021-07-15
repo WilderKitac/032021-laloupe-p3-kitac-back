5e22471fa1aa612812e4e2a86f5f476411c7e645
Damien Landouard <dlandouard@gmail.com>
2021-07-15 12:23:57 +0200

    pour fonctionnement de l'update avec ou sans image

--- src/routes/materials.routes.js
+++ src/routes/materials.routes.js
@@ -4,6 +4,7 @@ const {
   getOneMaterialsById,
   createOneMaterials,
   updateOneMaterials,
+  updateOneMatImg,
   deleteOneMaterials,
 } = require('../controllers/materials.controller');
 const { uploadImage } = require('../controllers/fileUpload.controller');
@@ -11,7 +12,7 @@ const { uploadImage } = require('../controllers/fileUpload.controller');
 materialsRouter.get('/', getAllMaterials);
 materialsRouter.get('/:id', getOneMaterialsById);
 materialsRouter.post('/', uploadImage, createOneMaterials, getOneMaterialsById);
-materialsRouter.post('/withImage/:id', uploadImage, updateOneMaterials, getOneMaterialsById);
+materialsRouter.post('/withImage/:id', uploadImage, updateOneMatImg, getOneMaterialsById);
 materialsRouter.put('/:id', updateOneMaterials, getOneMaterialsById);
 materialsRouter.delete('/:id', deleteOneMaterials);
 materialsRouter.post('/', createOneMaterials, getOneMaterialsById);
